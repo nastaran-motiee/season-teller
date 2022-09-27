24b3f02f559b1581a62b86d138c382c66312ce71
nnastaran83 <nnastaran83@gmail.com>
2022-09-27 17:50:00 +0300

    Class

--- src/index.js
+++ src/index.js
@@ -3,13 +3,31 @@ import { createRoot } from "react-dom/client"
 
 
 class App extends React.Component {
-    render() {
+    constructor (props) {
+        super(props);
+        this.state = { lat: null, errMessage: '' };
         navigator.geolocation.getCurrentPosition(
-            (position) => console.log(position),
-            err => console.log(err));
-        return <div> Latitude: </div>;
+            position => this.setState({ lat: position.coords.latitude }),
+            err => { this.setState({ errMessage: err.message }) }
+        );
+    }
+
+
+    render() {
+        if (this.state.errMessage && !this.state.lat) {
+            return <div> Error: { this.state.errMessage } </div>
+        }
+
+        if(!this.state.errMessage && this.state.lat){
+            return <div> Latitude: {this.state.lat} </div>
+        }
+
+        return <div> Loading! </div>
+
     }
 }
+
+
 const container = document.getElementById('root');
 const root = createRoot(container);
 root.render(<App />);
